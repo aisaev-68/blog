64b853fe0f236197282897f3bca9207c0fbb402a
ramazan_aisaev <aisaev-68@yandex.ru>
2023-12-24 10:22:07 +0300

    change views.py

--- auth_app/views.py
+++ auth_app/views.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from rest_framework.authtoken.models import Token
 from drf_yasg.utils import swagger_auto_schema
 
